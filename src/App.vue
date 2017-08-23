ad3ffd52e03aa9f997db449223d07ff4ea4b6865
Dylan_Macnab <dylanmacnab@gmail.com>
2017-08-22 23:09:31 -0700

    Linked firebase db and vue app together

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,37 @@
   <div id="app">
     <img src="./assets/logo.png">
     <hello></hello>
+    <ul>
+      <li v-for="flavor in flavors">{{flavor.name}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
 import Hello from './components/Hello'
+import Firebase from 'firebase'
+
+let config = {
+  apiKey: 'AIzaSyCJDGWXMLRlaTTbV_282w3JGTwivyIdSQE',
+  authDomain: 'lacroix-rankings.firebaseapp.com',
+  databaseURL: 'https://lacroix-rankings.firebaseio.com',
+  projectId: 'lacroix-rankings',
+  storageBucket: 'lacroix-rankings.appspot.com',
+  messagingSenderId: '675001064580'
+}
+
+let app = Firebase.initializeApp(config)
+let db = app.database()
+let flavorsRef = db.ref('flavors')
+console.log(flavorsRef)
 
 export default {
   name: 'app',
   components: {
     Hello
+  },
+  firebase: {
+    flavors: flavorsRef
   }
 }
 </script>
